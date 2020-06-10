d2af57787311e8efeb9d268f40dbe02cfca27c1c
Fraser Gyre <fraser.goldsworth@physics.ox.ac.uk>
2020-06-10 16:20:08 +0100

    fix: IMPORT FIX

--- slice_.py
+++ slice_.py
@@ -8,7 +8,7 @@ from threading import Thread
 from queue import Queue
 import time
 from multiprocessing import Pool
-from . import general as PVG
+import general as PVG
 
 
 def is_tile_in_slice(proc, tile, lat):
