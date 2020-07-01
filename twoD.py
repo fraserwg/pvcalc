1c370861cf00866f708d37ffd14c0ab8fe96e335
Fraser Gyre <fraser.goldsworth@physics.ox.ac.uk>
2020-07-01 14:31:43 +0100

    feat: FUNCTION TO POST PROCESS FILES ADDED

--- twoD.py
+++ twoD.py
@@ -93,3 +93,30 @@ def remove_land_points(ds, da_depth=None):
             pass
 
     return ds
+
+
+def post_process_files(in_fn, grid_fn, out_fn):
+    ''' applies post-processing to model output
+
+    Args:
+        in_fn --> path to netcdf file to be processed
+        grid_fn --> path to netcdf file containing grid information
+        out_fn --> path to save processed file to
+
+    Returns:
+        None
+
+    Notes:
+        - function removes y coordinates and land points, and formats the
+            level data of a raw MITgcm netcdf output file.
+        - The function could be modified to return the resulting dataset.
+    '''
+    # Open the ds and grid
+    ds = xr.open_dataset(in_fn)
+    grid = xr.open_dataset(grid_fn)
+
+    # Perform the processing
+    ds2d = PV2D.remove_y_coordinates(ds)
+    ds2d_noedge = PV2D.remove_land_points(ds)
+    ds2d_noedge_zformat = PVG.format_vertical_coordinates(ds, grid)
+    ds2d_noedge_zformat.to_netcdf(out_fn)
