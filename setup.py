5361c89af92636ca41d0440d2967b8d6e5797015
fraserwg <fraser.goldsworth@st-annes.ox.ac.uk>
2020-07-24 15:38:11 +0100

    updated setup.py to find subpackages

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
+import setuptools
 
 setup(
     name='PVCALC',
     version='0.0.1',
     author='Fraser William Goldsworth',
     author_email='fraser.goldsworth@physics.ox.ac.uk',
-    packages=['PVCALC'],
+    packages=setuptools.find_packages(),
+    #py_modules=['general'],
     scripts=['scripts/level_script.py', 'scripts/slice_script.py'],
     url='',  #'http://pypi.python.org/pypi/PackageName/',
     license='LICENSE.txt',
