2e6089ebbcdf3c1c3f133e3d1c6a51a19a826c0e
Fraser Goldsworth <fraser.goldsworth@env-res.ox.ac.uk>
2022-07-26 10:59:43 +0100

    ready for v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import setuptools
 
 setup(
-    name='PVCALC',
+    name='pvcalc',
     version='0.2.0',
     author='Fraser William Goldsworth',
     author_email='fraser.goldsworth@physics.ox.ac.uk',
@@ -13,5 +13,5 @@ setup(
     license='LICENSE.txt',
     description='A package for calculating PV from MITgcm outputs',
     long_description=open('README.md').read(),
-    install_requires=[],
+    install_requires=['xgcm'],
 )
